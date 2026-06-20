4574b65db2345e7841bfefd9f9ff97037c91d8c4
Ali Amin <90690340+Ali5723@users.noreply.github.com>
2026-06-20 20:18:28 +0300

    Update icon paths in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -62,13 +62,13 @@ export default defineConfig({
         start_url: "/",
         icons: [
           {
-            src: "icons/web-app-manifest-192x192.png",
+            src: "/icons/web-app-manifest-192x192.png",
             sizes: "192x192",
             type: "image/png",
             purpose: "any",
           },
           {
-            src: "icons/web-app-manifest-512x512.png",
+            src: "/icons/web-app-manifest-512x512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any",
